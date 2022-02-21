ff7985d681ff1bcf3648b6022322c0b42d9ef2c8
Murilo Kuerten <murilo.kuerten@vialaserdepilacao.com.br>
2022-02-21 06:50:07 -0300

    feat: add slider component

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { BackButton } from "../../components/BackButton";
+import { ImageSlider } from "../../components/ImageSlider";
 
-import { Container, Header } from "./styles";
+import { Container, Header, CarImages } from "./styles";
 
 export function CarDetails() {
   return (
     <Container>
       <Header>
-          <BackButton onPress={() => {}} />
+        <BackButton onPress={() => {}} />
       </Header>
+      <CarImages>
+        <ImageSlider
+          imagesUrl={[
+            "https://image.webmotors.com.br/_fotos/AnuncioUsados/gigante/2022/202201/20220126/audi-a8-6.3-fsi-longo-w12-48v-gasolina-4p-tiptronic-wmimagem09445290484.png",
+          ]}
+        />
+      </CarImages>
     </Container>
   );
 }
