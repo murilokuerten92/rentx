e9d936aed3f9220f672ff262125160e080a01003
Murilo Kuerten <murilo.kuerten@vialaserdepilacao.com.br>
2022-04-08 06:42:41 -0300

    feat: install lib

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import { Home } from '../screens/Home';
-import { CarDetails } from '../screens/CarDetails';
-import { Schedulling } from '../screens/Schedulling';
-import { SchedulingDetails } from '../screens/SchedulingDetails';
-import { SchedulingComplete } from '../screens/SchedulingComplete';
-import { Mycars } from '../screens/Mycars';
+import { Home } from "../screens/Home";
+import { CarDetails } from "../screens/CarDetails";
+import { Schedulling } from "../screens/Schedulling";
+import { SchedulingDetails } from "../screens/SchedulingDetails";
+import { SchedulingComplete } from "../screens/SchedulingComplete";
+import { Mycars } from "../screens/Mycars";
+import { Splash } from "../screens/Splash";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
-export function StackRoutes(){
-    return (
-        <Navigator screenOptions={{ headerShown: false }}>
-            <Screen name='Home' component={Home} />
-            <Screen name='CarDetails' component={CarDetails} />
-            <Screen name='Schedulling' component={Schedulling} />
-            <Screen name='SchedulingDetails' component={SchedulingDetails} />
-            <Screen name='SchedulingComplete' component={SchedulingComplete} />
-            <Screen name='MyCars' component={Mycars} />
-        </Navigator>
-    )
+export function StackRoutes() {
+  return (
+    <Navigator screenOptions={{ headerShown: false }} initialRouteName="Splash">
+      <Screen name="Splash" component={Splash} />
+      <Screen name="Home" component={Home} />
+      <Screen name="CarDetails" component={CarDetails} />
+      <Screen name="Schedulling" component={Schedulling} />
+      <Screen name="SchedulingDetails" component={SchedulingDetails} />
+      <Screen name="SchedulingComplete" component={SchedulingComplete} />
+      <Screen name="MyCars" component={Mycars} />
+    </Navigator>
+  );
 }
