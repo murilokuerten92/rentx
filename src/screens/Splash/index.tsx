8e119449da7d909d3086ceb0106b181c16ab6f30
Murilo Kuerten <murilo.kuerten@vialaserdepilacao.com.br>
2022-04-11 07:24:53 -0300

    feat: review animated function

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import {
- Container
-} from './styles';
+import Animated, { useSharedValue, useAnimatedStyle } from "react-native-reanimated";
+import { Button } from "../../components/Button";
+import { Container } from "./styles";
 
-export function Splash(){
- return (
+export function Splash() {
+  const animation = useSharedValue(0);
+
+  const animatedStyles = useAnimatedStyle(() => {
+      return {
+          transform: [{ translateX: animation.value }]
+      }
+  });
+
+  return (
     <Container>
-
+      <Animated.View style={animatedStyles}></Animated.View>
+      <Button title="Mover" onPress={() => {}} />
     </Container>
- );
+  );
 }
