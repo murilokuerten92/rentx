04efbbdde3d9f7318feaad203cfb84858b58a91f
Murilo Kuerten <murilo.kuerten@vialaserdepilacao.com.br>
2022-04-24 21:17:23 -0300

    feat: creacting buttons and titles

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -9,6 +9,10 @@ interface TouchableType extends TouchableOpacityProps {
     loading?: boolean;
 }
 
+interface ButtonTextProps {
+  light: boolean;
+}
+
 export const Container = styled.TouchableOpacity<TouchableType>`
   width: 100%;
 
@@ -19,8 +23,8 @@ export const Container = styled.TouchableOpacity<TouchableType>`
   background-color: ${props => props.color ? props.color : props.theme.colors.main};
   `;
 
-export const Title = styled.Text`
+export const Title = styled.Text<ButtonTextProps>`
  font-family: ${({ theme }) => theme.fonts.primary_500};
  font-size: ${RFValue(15)}px;
- color: ${({ theme }) => theme.colors.shape};
+ color: ${({ theme, light }) => light ? theme.colors.header : theme.colors.shape};
 `;
