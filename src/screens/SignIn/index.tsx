6ac427a2d732076e64831f867a0152e850d50d91
Murilo Kuerten <murilo.kuerten@vialaserdepilacao.com.br>
2022-04-28 06:37:10 -0300

    feat: creating acessibility of user

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 import { Container, Header, Title, SubTitle, Footer, Form } from "./styles";
-
+import {
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Button } from "../../components/Button";
 import { useTheme } from "styled-components";
@@ -12,35 +16,49 @@ export function SignIn() {
   const theme = useTheme();
 
   return (
-    <Container>
-      <StatusBar style="dark" backgroundColor="transparent" translucent />
-      <Header>
-        <Title>Estamos {"\n"}quase lá.</Title>
-        <SubTitle>
-          Faça seu login para começar {"\n"}
-          uma experiência incrível.
-        </SubTitle>
-      </Header>
-      <Form>
-        <Input iconName="mail" placeholder='E-mail' keyboardType='email-address' autoCorrect={false} autoCapitalize='none'  />
-        <PasswordInput iconName='lock' placeholder='Senha' secureTextEntry/>
-      </Form>
-      <Footer>
-        <Button
-          title="Login"
-          onPress={() => {}}
-          disabled={false}
-          loading={false}
-        />
-        <Button
-          title="Criar conta gratuita"
-          light
-          color={theme.colors.background_secondary}
-          onPress={() => {}}
-          disabled={false}
-          loading={false}
-        />
-      </Footer>
-    </Container>
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <StatusBar style="dark" backgroundColor="transparent" translucent />
+          <Header>
+            <Title>Estamos {"\n"}quase lá.</Title>
+            <SubTitle>
+              Faça seu login para começar {"\n"}
+              uma experiência incrível.
+            </SubTitle>
+          </Header>
+          <Form>
+            <Input
+              iconName="mail"
+              placeholder="E-mail"
+              keyboardType="email-address"
+              autoCorrect={false}
+              autoCapitalize="none"
+            />
+            <PasswordInput
+              iconName="lock"
+              placeholder="Senha"
+              secureTextEntry
+            />
+          </Form>
+          <Footer>
+            <Button
+              title="Login"
+              onPress={() => {}}
+              disabled={false}
+              loading={false}
+            />
+            <Button
+              title="Criar conta gratuita"
+              light
+              color={theme.colors.background_secondary}
+              onPress={() => {}}
+              disabled={false}
+              loading={false}
+            />
+          </Footer>
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -3,7 +3,6 @@ import styled from 'styled-components/native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
- flex: 1;
  padding: 0 24px;
 
  background-color: ${({ theme}) => theme.colors.background_secondary};
