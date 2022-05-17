dd588b18d93c2697b1f9eafd85852257662dc261
Murilo Kuerten <murilo.kuerten@vialaserdepilacao.com.br>
2022-05-17 04:58:10 -0300

    feat: persiste data in database

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,18 +1,21 @@
-import React, { createContext, useState, useContext, ReactNode } from "react";
+import React, {
+  createContext,
+  useState,
+  useContext,
+  ReactNode,
+  useEffect,
+} from "react";
 import { database } from "../database";
 import api from "../services/api";
-
+import { User as ModelUser } from "../database/model/User";
 interface User {
   id: string;
+  user_id: string;
   email: string;
   name: string;
   driver_license: string;
   avatar: string;
-}
-
-interface AuthState {
   token: string;
-  user: User;
 }
 
 interface SignInCredentials {
@@ -32,7 +35,23 @@ interface AuthProviderProps {
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
-  const [data, setData] = useState<AuthState>({} as AuthState);
+  const [data, setData] = useState<User>({} as User);
+
+  useEffect(() => {
+    (async () => {
+      const userCollection = database.get<ModelUser>("users");
+
+      const response = await userCollection.query().fetch();
+
+      if (response.length > 0) {
+        const userData = response[0]._raw as unknown as User;
+        api.defaults.headers.common[
+          "Authorization"
+        ] = `Bearer ${userData.token}`;
+        setData(userData);
+      }
+    })();
+  }, []);
 
   async function signIn({ email, password }: SignInCredentials) {
     try {
@@ -44,13 +63,20 @@ function AuthProvider({ children }: AuthProviderProps) {
       const { token, user } = response.data;
       api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
-      const userCollection = database.get('users');
+      const userCollection = database.get<ModelUser>("users");
 
-      await database.(async () => {
+      await database.write(async () => {
+        await userCollection.create((newUser) => {
+          newUser.user_id = user.id;
+          newUser.name = user.name;
+          newUser.email = user.email;
+          newUser.driver_license = user.driver_license;
+          newUser.avatar = user.avatar;
+          newUser.token = token;
+        });
+      });
 
-      })
-
-      setData({ token, user });
+      setData({ ...user, token });
     } catch (error) {
       throw new Error(error);
     }
@@ -59,7 +85,7 @@ function AuthProvider({ children }: AuthProviderProps) {
   return (
     <AuthContext.Provider
       value={{
-        user: data.user,
+        user: data,
         signIn,
       }}
     >
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,8 +11,7 @@ export function Routes() {
 
   return (
     <NavigationContainer>
-      {user ?  <AppTabRoutes /> : <AuthRoutes />}
-     
+      {user.id ?  <AppTabRoutes /> : <AuthRoutes />}
     </NavigationContainer>
   );
 }
